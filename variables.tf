bf09e896ff3ceffac17fd017b2e84dcc902d182a
wfishburn1 <137707895+wfishburn1@users.noreply.github.com>
2026-02-19 11:25:40 -0500

    Refactor variables in variables.tf file

--- variables.tf
+++ variables.tf
@@ -1,61 +1,10 @@
 #credentials
-variable "subscription_id" {
+variable "name" {
   type = string
+  default = "module_rg"
 }
-variable "client_id" {
-  type = string
-}
-variable "client_secret" {
-  type = string
-}
-variable "tenant_id" {
-  type = string
-}
-
-#resource group variables
-variable "RG_name" {
-  type = string
-}
-
 variable "location" {
   type = string
+  default = "eastus"
 }
 
-#Networking variables
-variable "demo_network_NSG" {
-  type = string
-}
-variable "network_name" {
-  type = string
-}
-
-variable "address_space" {
-  type = list(string)
-}
-variable "subnet1" {
-  type = string
-}
-
-variable "subnet_address" {
-  type = string
-}
-
-variable "subnet2" {
-  type = string
-}
-
-variable "subnet2_address" {
-  type = string
-}
-
-variable "secret_rg_name" {
-  type = string
-}
-
-variable "secret_vault_name" {
-  type = string
-}
-
-variable "tags" {
-  type = map(string)
-}
